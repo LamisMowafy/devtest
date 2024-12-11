39cb3885294444447a3ef0cf6d1f8eea11e6ba4d
engdelta588 <englamis@ymail.com>
2024-12-11 16:15:13 +0200

    Initial commit

---
+++ Task/Models/MetadataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Task.Models
+{
+    public class MetadataModel
+    {
+        public string? Title { get; set; } 
+        public string? Author { get; set; }
+        public string? CreationDate { get; set; }
+        public bool MissingMetadata { get; set; }
+    }
+}
---
+++ Task/Models/Reports.cs
@@ -0,0 +1,9 @@
+﻿namespace Task.Models
+{
+    public class Reports
+    {
+        public List<string> ProcessedFiles { get; set; } = new List<string>();
+        public List<string> FilesWithMissingMetadata { get; set; } = new List<string>();
+        public int TotalWordCount { get; set; } = 0;
+    }
+}
